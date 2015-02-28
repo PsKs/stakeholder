c2846dec6e4e59c5f747c3762607ed65c28047d2
PsKs <6e:82:ac:a1:2c:5d:a2:7d:da:ab:42:b4:28:1c:2d:b0>
2015-03-01 03:57:35 +0700

    เพิ่ม form swot tows
    แก้ไขให้เลือกใช้ form ปกติกับ swot

---
+++ user/default_form.php
@@ -0,0 +1,63 @@
+<table class="table table-striped table-bordered table-hover">
+  <thead>
+    <tr>
+    <?php
+      foreach ($arr_stklist_name as $key => $value) {
+        echo "<th class='text-center'>".$value."</th>";
+      }
+    ?>
+    <th class='col-md-1'></th>
+    </tr>
+  </thead>
+  <tbody id='p_scents'>
+    <script>
+      var scntDiv = $('#p_scents'),
+          i = $('#p_scents tr').size() + 1,
+          row = 0,
+          arr_stklist = <?php echo json_encode($arr_stklist_type) ?>,
+          form = [],
+          x = [];
+      function gen_form() {
+        arr_stklist.forEach(function(element, index){
+          if (element == "text") {
+            form[index] = '<td><textarea class="form-control" rows="1" id="arr_TextAns['+row+']['+index+']"></textarea></td>';
+          } else if (element == "level") {
+            form[index] = '<td><select class="form-control" id="arr_LevelAns['+row+']['+index+']">\
+                              <option value="1" selected="selected">1</option>\
+                              <option value="2">2</option>\
+                              <option value="3">3</option>\
+                              <option value="4">4</option>\
+                              <option value="5">5</option></td>';
+          } else if (element == "sum") {
+            form[index] = '<td><p id="arr_Result['+row+']['+index+']" class="form-control-static text-center"></p></td>';
+          }
+        });
+        row++;
+        return form;
+      }
+      function get_form() {
+        scntDiv.append('<tr>'+gen_form()+'</tr>');
+      }            
+    </script>
+    <?php
+      echo  '<script type="text/javascript">',
+            'get_form();',
+            '</script>';
+      /*foreach ($arr_stklist_type as $key => $value) {
+        if ($value == "text") {
+          echo $form[] = "<td><input type='text' class='form-control' name='arr_TextAns[][$key]'/>";
+        } elseif ($value == "level") {
+          echo $form[] = "<td><select class='form-control' name='arr_LevelAns[][$key]'>
+                          <option value='-'>SELECT</option>
+                          <option value='1'>1</option>
+                          <option value='2'>2</option>
+                          <option value='3'>3</option>
+                          <option value='4'>4</option>
+                          <option value='5'>5</option>";
+        } else {
+          echo $form[] = "<td><p id='arr_Result[][$key]' class='form-control-static'></p>";
+        }
+      }*/
+    ?>            
+  </tbody>
+</table>
